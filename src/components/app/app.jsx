78935020d74facb8cefeb5c21d52dbd02605f253
korbutds <sys@hohou.ru>
2021-03-29 18:20:56 +0300

    feat: Подготовит проект

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='outside-project'>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default App;

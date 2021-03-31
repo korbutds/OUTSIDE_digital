9ab53390a79a6f9816aea35851e5192d37338ba5
korbutds <sys@hohou.ru>
2021-03-31 11:15:13 +0300

    feat: Добавит тест компоненту CaclulateForm

---
+++ src/components/calculate-form/calculate-form.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import CalculateForm from './calculate-form';
+
+it('Renders Calculate form correctly', () => {
+  render(<CalculateForm />);
+  expect(screen.getByText('Введите данные')).toBeInTheDocument();
+});

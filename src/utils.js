f2883975b9513a97a57c777225e1506f8b95cbe5
korbutds <sys@hohou.ru>
2021-03-31 18:21:36 +0300

    refactor: Добавит тесты

--- src/utils.js
+++ src/utils.js
@@ -1,35 +1,42 @@
 import { MAX_PROPERTY_COST, VAT } from "./const";
 
-export const calculatePaymentArray = (salary) => {
-  const paymentArray = [];
-  const yearSalary = salary * 12;
-  const yearTax = Math.floor(yearSalary * VAT / 100);
-  let taxDeduction = MAX_PROPERTY_COST * VAT / 100;
+export const calculatePaymentArray = (monthSalary) => {
+  const salary = Number(monthSalary);
+  if (salary) {
+    const paymentArray = [];
+    const yearSalary = salary * 12;
+    const yearTax = Math.floor(yearSalary * VAT / 100);
+    let taxDeduction = MAX_PROPERTY_COST * VAT / 100;
 
-  while (taxDeduction > 0) {
-  const sum = (taxDeduction > yearTax) ? yearTax : taxDeduction;
-  paymentArray.push(sum)
-  taxDeduction -= yearTax;
+    while (taxDeduction > 0) {
+    const sum = (taxDeduction > yearTax) ? yearTax : taxDeduction;
+    paymentArray.push(sum);
+    taxDeduction -= yearTax;
+    }
+
+    return paymentArray;
   }
-
-  return paymentArray
+  return [];
 }
 
 export const getNumberEnd = (number) => {
-  const numberEnd = (number) => {
-    let end = ``;
-    const lastNumber = number.toString();
-    if (lastNumber.endsWith(`3`)) {
-      end = `ий`
-    } else if (lastNumber.endsWith(`2`) || lastNumber.endsWith(`6`) || lastNumber.endsWith(`7`) || lastNumber.endsWith(`8`)) {
-      end = `ой`;
-    } else {
-      end = `ый`;
+  const validNumber = !!Number(number);
+  if (validNumber) {
+    const numberEnd = (number) => {
+      const numberStr = number.toString();
+      if (numberStr.endsWith(`3`)) {
+        return `ий`
+      } else if (numberStr.endsWith(`2`) || numberStr.endsWith(`6`) || numberStr.endsWith(`7`) || numberStr.endsWith(`8`)) {
+        return `ой`;
+      } else {
+        return `ый`;
+      }
     }
-  return end;
+
+    return (number > 10 && number < 20) ? `ый` : numberEnd(number);
   }
 
-  return (number > 10 && number < 20) ? `ый` : numberEnd(number);
+  return ``;
 }
 
 export const calculateExp = (year) => {
